a3fe80f739cfaf738de79ac6841c3c6a640a24db
Yuwen Ma <yuwenma@google.com>
2022-08-19 16:10:14 -0700

    ini

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_ID=$(gcloud config get-value project)
+FOLDER_ID=$(gcloud projects describe ${PROJECT_ID} "--format=get(parent.id)")
+
+kubectl apply -f - << EOF
+apiVersion: resourcemanager.cnrm.cloud.google.com/v1beta1
+kind: Folder
+metadata:
+  name: root
+  namespace: config-control
+  annotations:
+    cnrm.cloud.google.com/deletion-policy: "abandon"
+spec:
+  displayName: "root"
+  folderRef:
+    external: "${FOLDER_ID}"
+EOF
